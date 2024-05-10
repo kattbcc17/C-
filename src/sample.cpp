40ac2492bb10688ddfe0851e73081ed4c3f27212
kattbcc17 <kattbcc17@byui.edu>
2024-05-09 20:34:52 -0500

    first program

---
+++ src/sample.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // Prints out on a different line 
+    // cout << "hello world!" << endl << "my first try with c++";
+    // cout << "hello world!" << " My first try with c++";
+    
+    int numero_entero = 70;
+    double decimal = 23.78;
+    char example = 'a';
+    //print out 
+    cout << numero_entero <<endl; // salto de linea
+    cout << decimal << endl;
+    cout << example ;
+    
+    return 0;
+}
