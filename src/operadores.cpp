bded82a167ab389453a530d51a19a3936aa66abe
kattbcc17 <kattbcc17@byui.edu>
2024-05-09 20:57:18 -0500

    operaciones

---
+++ src/operadores.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // int a = 8 , b = 2, add; // put in same line because they share the same type
+    // int add;
+
+    // add = a + b;
+
+    // cout << "The result is: " << a % b;
+    // cout << "The result is: " << add;
+
+    int a = 10;
+    int result_a;
+
+    // incrementar variable a
+    // result_a = ++a;
+    result_a = --a;
+
+    cout << "Now the variable is: " << result_a;
+
+    return 0;
+}
---
+++ src/operators.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // operadores de asignacion
+    // int a,b, edad;
+
+    // a = 8;
+    // b = 7;
+    // edad = a;
+
+    // cout << "AGE IS: " <<edad << endl;
+    // a = a + b;
+    // simplify
+    // a += b; // a = a + b
+    // cout << "a = "<< a;
+
+    int n1, n2, suma;
+    cout << "ingrese un numero: ";
+    cin >> n1;
+    cout << "ingrese otro numero: ";
+    cin >> n2; 
+    suma = n1 + n2;
+
+    cout << "la suma es: " << suma;
+
+    return 0;
+}
